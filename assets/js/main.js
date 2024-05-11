4f65de4f55f9dda94fdee7210d1ea44285a3c307
AbaevB <abaev70@hotmail.com>
2024-05-12 02:07:36 +0700

    feat(api) yandex map

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,31 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+//map
+ymaps.ready(init);
+	var myMap;
+
+	function init() {
+
+		myMap = new ymaps.Map("map", {
+			center: [56.49310906823801,84.89373199999991], // Координаты центра карты
+			zoom: 15 // Маштаб карты
+		});
+
+		myMap.controls.add(
+			new ymaps.control.ZoomControl()  // Добавление элемента управления картой
+		);
+
+		myPlacemark = new ymaps.Placemark([56.49310906823801,84.89373199999991], { // Координаты метки объекта
+			balloonContent: '<div class="ya_map"><h3>Lady Selena</h3><p>Деревянная мебель на заказ. Эксклюзивный дизайн</p></div><img src="assets/img/selena_black.png"/>', // Подсказка метки
+      iconContent: 'Lady Selena'
+		}, {
+			preset: "islands#greenStretchyIcon", // Тип метки
+      balloonCloseButton: true,
+      hideIconOnBalloonOpen: false
+		});
+
+		myMap.geoObjects.add(myPlacemark); // Добавление метки
+		myPlacemark.balloon.close(); // Открытие подсказки метки
+
+	};
